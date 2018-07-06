fa3f42f5a500623aea123b1bb86c8cc2d1782aae
zhxh <zhxhcoder@gmail.com>
2018-07-06 10:26:08 +0800

    CacheUtils

--- app/src/main/java/com/zhxh/xutils/MainActivity.java
+++ app/src/main/java/com/zhxh/xutils/MainActivity.java
@@ -1,13 +1,25 @@
 package com.zhxh.xutils;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_show = findViewById(R.id.tv_show);
+
+        String test = "";
+
+        //for (Application application : AppInfoUtils.getAllNonSystemApplication(this)) {
+        //    test = "\n" + application.getPackageName() + test;
+        //}
+
+        tv_show.setText(test);
     }
 }
---
+++ xutilslib/src/main/java/com/zhxh/xutilslib/AppInfoUtils.java
@@ -0,0 +1,108 @@
+package com.zhxh.xutilslib;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhxh on 2018/7/6
+ */
+public class AppInfoUtils {
+
+    private enum ApplicationType {
+        //所有应用、非系统应用、系统应用
+        allApplication, nonSystemApplication, systemApplication
+    }
+
+    /**
+     * 获取设备的应用信息
+     */
+    private static List<Application> getApplicationInfo(Context context, ApplicationType applicationType) {
+        List<Application> applicationList = new ArrayList<>();
+        List<PackageInfo> packageInfoList = context.getPackageManager().getInstalledPackages(0);
+        for (PackageInfo packageInfo : packageInfoList) {
+
+            Application application = new Application();
+            application.getApplicationInfo().loadLabel(context.getPackageManager());
+            application.getApplicationInfo().loadIcon(context.getPackageManager());
+            application.getApplicationInfo().packageName = packageInfo.packageName;
+            application.getApplicationInfo().name = packageInfo.versionName;
+
+            switch (applicationType) {
+                case allApplication:
+                    applicationList.add(application);
+                    break;
+                case systemApplication:
+                    if (isSystemApplication(packageInfo)) {
+                        applicationList.add(application);
+                    }
+                    break;
+                case nonSystemApplication:
+                    if (!isSystemApplication(packageInfo)) {
+                        applicationList.add(application);
+                    }
+                    break;
+            }
+        }
+        return applicationList;
+    }
+
+    /**
+     * 判断是否是系统应用
+     */
+    private static Boolean isSystemApplication(PackageInfo packageInfo) {
+        return (packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+    }
+
+    /**
+     * 获取设备所有的应用
+     */
+    public static List<Application> getAllApplication(Context context) {
+        return getApplicationInfo(context, ApplicationType.allApplication);
+    }
+
+    /**
+     * 获取设备所有的系统应用
+     */
+    public static List<Application> getAllSystemApplication(Context context) {
+        return getApplicationInfo(context, ApplicationType.systemApplication);
+    }
+
+    /**
+     * 获取设备所有的非系统应用
+     */
+    public static List<Application> getAllNonSystemApplication(Context context) {
+        return getApplicationInfo(context, ApplicationType.nonSystemApplication);
+    }
+
+    /**
+     * 根据应用包名获取应用名
+     */
+    public static String getApplicationNameByPackageName(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            return pm.getApplicationLabel(pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA)).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 判断指定包名的应用是否存在
+     */
+    public static boolean appExist(Context context, String packageName) {
+        List<PackageInfo> packageInfoList = context.getPackageManager().getInstalledPackages(0);
+        for (PackageInfo packageInfo : packageInfoList) {
+            if (packageInfo.packageName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
